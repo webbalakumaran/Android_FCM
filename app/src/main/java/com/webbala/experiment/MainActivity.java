828a41b4198b1848091709bed039602740c95f3a
webbalakumaran <webbala006@gmail.com>
2022-04-27 10:16:32 +0530

    Completed Firestore trigger with push notification

--- app/src/main/java/com/webbala/experiment/MainActivity.java
+++ app/src/main/java/com/webbala/experiment/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging firebaseMessaging = FirebaseMessaging.getInstance();
+        firebaseMessaging.subscribeToTopic("new_user_forums");
     }
 }
